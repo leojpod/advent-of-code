ea8557bd1ebf2c3b37d6e84ce24893cca636a6ef
Leo jPod <leo.jpod@lilo.org>
2019-12-03 08:36:50 +0100

    improve basic output

--- 2019/src/main.js
+++ 2019/src/main.js
@@ -3,12 +3,12 @@ const program = require("./elm.js").Elm.Main.init({
   flags: { argv: process.argv, versionMessage: "0.1" }
 });
 
-program.ports.print.subscribe(message => console.log(message));
+program.ports.print.subscribe(message => console.log("INFO: ", message));
 program.ports.printAndExitFailure.subscribe(message => {
-  console.log(message);
+  console.error("FAILURE: ", message);
   process.exit(1);
 });
 program.ports.printAndExitSuccess.subscribe(message => {
-  console.log(message);
+  console.log("SUCCESS: ", message);
   process.exit(0);
 });
